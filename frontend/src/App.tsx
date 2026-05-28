bc104abe23340870801ceaeb334dd8345684ecf4
Vanessa <vs6615302@gmail.com>
2026-05-27 21:34:57 -0300

    fix: corrigir estrutura de pastas e teste da logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import Login from "./auth/Login"
 
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Login/>
     </>
   )
 }
---
+++ frontend/src/auth/Login.tsx
@@ -0,0 +1,11 @@
+import logoDevboard from "../assets/imagens/logo-devboard.png"
+
+function Login() {
+  return (
+    <div>
+      <img src={logoDevboard} alt="Logo DevBoard" />
+    </div>
+  )
+}
+
+export default Login
